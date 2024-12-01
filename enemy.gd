9f5e64a257b5f0fedbf2058a3239ebcb72da80f2
M_TTI <martin.bonetti.dev@gmail.com>
2024-12-01 19:19:12 +0100

    updated .gitignore

--- enemy.gd
+++ enemy.gd
@@ -3,8 +3,7 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var mob_types = $EnemySprite.sprite_frames.get_animation_names()
-	$EnemySprite
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
