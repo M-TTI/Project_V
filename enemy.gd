ad049abac15f0c3dc5931c4e4e368046dc17d783
M_TTI <martin.bonetti.dev@gmail.com>
2024-12-01 17:36:29 +0100

    did player and enemy

---
+++ enemy.gd
@@ -0,0 +1,12 @@
+extends RigidBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var mob_types = $EnemySprite.sprite_frames.get_animation_names()
+	$EnemySprite
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
